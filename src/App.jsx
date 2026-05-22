1e4488fbe897dafb02f6a1cb37a7c9ef75ee25cd
Joshu2001 <jshsajous@gmail.com>
2026-05-22 11:54:58 +0800

    Restore assistant Run Assist box style without shadow

--- src/App.jsx
+++ src/App.jsx
@@ -5158,7 +5158,7 @@ Rules:
             </div>
           </form>
           </div>
-        </div>
+                  className="bg-white rounded-2xl p-2.5 border border-violet-100 space-y-2 pointer-events-auto"
 
         {!isComposing && (
           <div
