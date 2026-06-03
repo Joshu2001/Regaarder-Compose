8cd8605695cc358ac30a79c68658df2af8c3eae2
Joshu2001 <jshsajous@gmail.com>
2026-06-03 16:52:42 +0800

    Add private Analytics page and route (/analytics)

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
+import Analytics from './Analytics.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const pathname = typeof window !== 'undefined' ? window.location.pathname : '';
+
+if (pathname === '/analytics') {
+  root.render(
+    <React.StrictMode>
+      <Analytics />
+    </React.StrictMode>,
+  );
+} else {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+}
